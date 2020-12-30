c5930414d2bcd42f99d11b8d09bae68e9da1da98
LukeLIN-web <1263810658@qq.com>
2020-12-31 00:13:44 +0800

    fangtang something , sorting out database....  create database table ts user ,maybe just create class manually,  I will debug in the database project, then copy to fangtang. id as primary key, name , password, name can be show but log in with id and password.  I will create class to add password and name in database, delete it, find id and compare input password with password, change password.

--- FangTchat/src/fangT/FangTclient.java
+++ FangTchat/src/fangT/FangTclient.java
@@ -1,8 +1,4 @@
 package fangT;
-/*
-* TCPClientThreadFX.java
-* Copyright (c) 2020-10-25
-*/
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -17,6 +13,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
@@ -25,156 +22,143 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-
-
 public class FangTclient extends Application {
+	
+	private Button btnExit=new Button("退出");
+	private Button btnSend = new Button("发送");
+	private TextField tfSend=new TextField();//输入信息区域
+	private TextArea taDisplay = new TextArea();//显示区域
+	private TextField username = new TextField();//填写用户名
+	private TextField password = new TextField();//填写密码
+	private Button btConn=new Button("连接");
+	private TCPClient tcpClient;
+	private Thread readThread;
+		
+	public static void main(String[] args) {
+		launch(args);//用来启动整个Application的
+	}
+	
+	@Override
+	public void start(Stage primaryStage) { //start()方法传入的Stage对象在JavaFX程序加载时被加载（JavaFX的Stage是顶层容器）
+		BorderPane mainPane=new BorderPane();//BorderPane，是默认就分割好了上下左右中的五个部分
+		//连接服务器区域
+		HBox hBox1=new HBox();
+		hBox1.setSpacing(10);
+		hBox1.setPadding(new Insets(10,20,10,20));
+		hBox1.setAlignment(Pos.CENTER);
+		hBox1.getChildren().addAll(new Label("用户名："),username,new Label("密码："),password,btConn);// 从左到右horizon
+		mainPane.setTop(hBox1);// 放上方
+		
+		VBox vBox=new VBox();//vertical box 
+		vBox.setSpacing(10);
+		vBox.setPadding(new Insets(10,20,10,20));
+		vBox.getChildren().addAll(new javafx.scene.control.Label("信息显示区"),taDisplay,new Label("信息输入区"),tfSend);
+		VBox.setVgrow(taDisplay, Priority.ALWAYS);
+		mainPane.setCenter(vBox);//居中
+		
+		HBox hBox=new HBox();
+		hBox.setSpacing(10);
+		hBox.setPadding(new Insets(10,20,10,20));
+		hBox.setAlignment(Pos.CENTER_RIGHT);
+		hBox.getChildren().addAll(btnSend,btnExit);
+		mainPane.setBottom(hBox);//下方 
+		
+		Scene scene =new Scene(mainPane,700,500);
+		primaryStage.setScene(scene);// Place the scene in the stage
+		primaryStage.show();// Display the stage
+		primaryStage.setTitle("FangTangChatRoom");// Set the stage title
+		primaryStage.getIcons().add(new Image("LOGO2.png"));
+		//连接按钮
+		btConn.setOnAction(event -> {
+		String usr = username.getText().trim();
+		String pwd= password.getText().trim();
+		
+			try {
+			//tcpClient是本程序定义的一个TCPClient类型的成员变量
+			tcpClient = new TCPClient("localhost", "9020");
+			//用于接收服务器信息的单独线程
+			readThread = new Thread(()->{
+				String receiveMsg=null;//从服务器接收一串字符
+				while ((receiveMsg=tcpClient.receive())!=null){
+					//lambda表达式不能直接访问外部非final类型局部变量，需要定义一个临时变量,若将receiveMsg定义为类成员变量，则无需临时变量
+					String msgTemp = receiveMsg;
+					Platform.runLater(()->{
+						LocalDateTime now = LocalDateTime.now();
+						DateTimeFormatter dtf= DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss") ;
+						taDisplay.appendText(dtf.format(now)+"\n");// format print
+						taDisplay.setStyle("-fx-text-fill:black");
+						taDisplay.appendText(msgTemp+"\n");
+					});
+				}
+				Platform.runLater(()->{
+					taDisplay.appendText("对话已关闭！\n");
+				});
+			});
+				readThread.start();			
+				btConn.setDisable(true);	//连接服务器之后未结束服务前禁用再次连接
+				tfSend.setDisable(false);	//重新连接服务器时启用输入发送功能
+				btnSend.setDisable(false);
+			}catch (Exception e){
+				taDisplay.appendText("服务器连接失败！"+e.getMessage()+"\n");
+			}
+		});
+	// btConn.defaultButtonProperty();
+	//发送按钮事件
+	btnSend.setOnAction(event -> {
+		String msg=tfSend.getText();
+		tcpClient.send(msg);//向服务器发送一串字符
+		taDisplay.appendText("客户端发送："+msg+"\n");
+		if (msg.equalsIgnoreCase("bye")){
+			btnSend.setDisable(true);//发送bye后禁用发送按钮
+			tfSend.setDisable(true);//禁用Enter发送信息输入区域
+			//结束服务后再次启用连接按钮
+			btConn.setDisable(false);
+		}
+		tfSend.clear();
+	});
+	//对输入区域绑定键盘事件
+	tfSend.setOnKeyPressed(new EventHandler<KeyEvent>() {
+		@Override
+		public void handle(KeyEvent event) {
+			if(event.getCode()==KeyCode.ENTER){
+				String msg=tfSend.getText();
+				tcpClient.send(msg);//向服务器发送一串字符
+				taDisplay.appendText("客户端发送："+msg+"\n");
+				
+				if (msg.equalsIgnoreCase("bye")){
+					tfSend.setDisable(true);//禁用Enter发送信息输入区域
+					btnSend.setDisable(true);//发送bye后禁用发送按钮
+					//结束服务后再次启用连接按钮
+					btConn.setDisable(false);
+				}
+				tfSend.clear();
+			}
+		}
+	});
+	
+		btnExit.setOnAction(event -> {
+			try {
+				exit();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+	//窗体关闭响应的事件,点击右上角的×关闭,客户端也关闭
+		primaryStage.setOnCloseRequest(event -> {
+			try {
+				exit();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+	}
 
-private Button btnExit=new Button("退出");
-private Button btnSend = new Button("发送");
-
-private TextField tfSend=new TextField();//输入信息区域
-
-private TextArea taDisplay=new TextArea();//显示区域
-private TextField ipAddress=new TextField();//填写ip地址
-private TextField tfport=new TextField();//填写端口
-private Button btConn=new Button("连接");
-private TCPClient tcpClient;
-private Thread readThread;
-
-public static void main(String[] args) {
-launch(args);
-}
-
-@Override
-public void start(Stage primaryStage) {
-BorderPane mainPane=new BorderPane();
-
-//连接服务器区域
-HBox hBox1=new HBox();
-hBox1.setSpacing(10);
-hBox1.setPadding(new Insets(10,20,10,20));
-hBox1.setAlignment(Pos.CENTER);
-hBox1.getChildren().addAll(new Label("ip地址："),ipAddress,new Label("端口："),tfport,btConn);
-mainPane.setTop(hBox1);
-
-VBox vBox=new VBox();
-vBox.setSpacing(10);
-
-vBox.setPadding(new Insets(10,20,10,20));
-vBox.getChildren().addAll(new javafx.scene.control.Label("信息显示区"),taDisplay,new Label("信息输入区"),tfSend);
-
-VBox.setVgrow(taDisplay, Priority.ALWAYS);
-mainPane.setCenter(vBox);
-
-
-
-HBox hBox=new HBox();
-hBox.setSpacing(10);
-hBox.setPadding(new Insets(10,20,10,20));
-hBox.setAlignment(Pos.CENTER_RIGHT);
-hBox.getChildren().addAll(btnSend,btnExit);
-mainPane.setBottom(hBox);
-
-Scene scene =new Scene(mainPane,700,500);
-primaryStage.setScene(scene);
-primaryStage.show();
-//连接按钮
-btConn.setOnAction(event -> {
-//String ip=ipAddress.getText().trim();
-//String port=tfport.getText().trim();
-
-try {
-//tcpClient是本程序定义的一个TCPClient类型的成员变量
-tcpClient = new TCPClient("localhost", "9020");
-//用于接收服务器信息的单独线程
-readThread = new Thread(()->{
-String receiveMsg=null;//从服务器接收一串字符
-while ((receiveMsg=tcpClient.receive())!=null){
-//lambda表达式不能直接访问外部非final类型局部变量，需要定义一个临时变量
-//若将receiveMsg定义为类成员变量，则无需临时变量
-String msgTemp = receiveMsg;
-Platform.runLater(()->{
-	LocalDateTime now = LocalDateTime.now();
-	DateTimeFormatter dtf= DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss") ;
-	taDisplay.appendText(dtf.format(now)+"\n");// format print
-	taDisplay.setStyle("-fx-text-fill:black");
-	taDisplay.appendText(msgTemp+"\n");
-});
-}
-Platform.runLater(()->{
-taDisplay.appendText("对话已关闭！\n");
-});
-});
-
-readThread.start();
-//连接服务器之后未结束服务前禁用再次连接
-btConn.setDisable(true);
-//重新连接服务器时启用输入发送功能
-tfSend.setDisable(false);
-btnSend.setDisable(false);
-}catch (Exception e){
-taDisplay.appendText("服务器连接失败！"+e.getMessage()+"\n");
-}
-});
-
-// btConn.defaultButtonProperty();
-
-//发送按钮事件
-btnSend.setOnAction(event -> {
-String msg=tfSend.getText();
-tcpClient.send(msg);//向服务器发送一串字符
-taDisplay.appendText("客户端发送："+msg+"\n");
-if (msg.equalsIgnoreCase("bye")){
-btnSend.setDisable(true);//发送bye后禁用发送按钮
-tfSend.setDisable(true);//禁用Enter发送信息输入区域
-//结束服务后再次启用连接按钮
-btConn.setDisable(false);
-}
-tfSend.clear();
-});
-//对输入区域绑定键盘事件
-tfSend.setOnKeyPressed(new EventHandler<KeyEvent>() {
-@Override
-public void handle(KeyEvent event) {
-if(event.getCode()==KeyCode.ENTER){
-String msg=tfSend.getText();
-tcpClient.send(msg);//向服务器发送一串字符
-taDisplay.appendText("客户端发送："+msg+"\n");
-
-if (msg.equalsIgnoreCase("bye")){
-tfSend.setDisable(true);//禁用Enter发送信息输入区域
-btnSend.setDisable(true);//发送bye后禁用发送按钮
-//结束服务后再次启用连接按钮
-btConn.setDisable(false);
-}
-tfSend.clear();
-}
-}
-});
-
-btnExit.setOnAction(event -> {
-try {
-exit();
-} catch (InterruptedException e) {
-e.printStackTrace();
-}
-});
-//窗体关闭响应的事件,点击右上角的×关闭,客户端也关闭
-primaryStage.setOnCloseRequest(event -> {
-try {
-exit();
-} catch (InterruptedException e) {
-e.printStackTrace();
-}
-});
-}
-
-private void exit() throws InterruptedException {
-if (tcpClient!=null){
-tcpClient.send("bye");
-Thread.sleep(1000);//多线程等待，关闭窗口时还有线程等待IO，设置1s间隔保证所有线程已关闭
-tcpClient.close();
-}
-System.exit(0);
-}
+	private void exit() throws InterruptedException {
+		if (tcpClient!=null){
+			tcpClient.send("bye");
+			Thread.sleep(1000);//多线程等待，关闭窗口时还有线程等待IO，设置1s间隔保证所有线程已关闭
+			tcpClient.close();
+		}
+		System.exit(0);
+	}
 }
--- FangTchat/src/fangT/FtServer.java
+++ FangTchat/src/fangT/FtServer.java
@@ -3,171 +3,152 @@ package fangT;
 import java.io.*;
 import java.net.*;
 import java.util.*;
-import java.util.concurrent.*;
+import java.util.concurrent.*;//实现线程安全. 
 //import java.util.concurrent.ExecutorService;
 //import java.util.concurrent.Executors;
 
 public class FtServer {
-	private ConcurrentHashMap<Socket, String> users = new ConcurrentHashMap<Socket, String>();
+	private ConcurrentHashMap<Socket, String> users = new ConcurrentHashMap<Socket, String>();//save user name and socket
 	String localName = null;
 	String hostName;
 	boolean flag = false ; // in the method init flag will occur error 匿名内部类和局部内部类在初始化后，又对这个变量进行了赋值。赋值后会认为这个变量不是final了，所以报错
 	public static void main(String[] args){
-		
 			try {
 				new FtServer().Service();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				e.printStackTrace();// maybe you can show message JoptionPane
 			}
 	}
-
-private int port = 9020;
-
-private ServerSocket serverSocket;//定义服务器套接字
-private ExecutorService executorService = Executors.newCachedThreadPool();
-
-	public FtServer() throws IOException{
-		
-serverSocket =new ServerSocket(port);
-System.out.println("服务器启动监听在"+port+"端口...");  //constructor init the serversocket
-}
-// can send and receive
+	private int port = 9020;
+	private ServerSocket serverSocket;//定义服务器套接字
+	private ExecutorService executorService = Executors.newCachedThreadPool();
+	public FtServer() throws IOException{		
+		serverSocket =new ServerSocket(port);
+		System.out.println("服务器启动监听在"+port+"端口...");  //constructor init the serversocket
+	}
+	// can send and receive
 	class Handler implements Runnable{
 		private Socket socket;
 		boolean isExist;
 		public Handler(Socket socket) {
-		this.socket = socket;
+			this.socket = socket;
 		}
 		//判断用户是否已经下线
 		private Boolean isLeaved(Socket temp){
-		Boolean leave=true;
-		for(Map.Entry<Socket,String> mapEntry : users.entrySet()) {
-		if (mapEntry.getKey().equals(temp))
-		leave = false;
-		}
-		return leave;
-		}
-		
-		public void run() {
-		//本地服务器控制台显示客户端连接的用户信息
-		System.out.println("New connection accept:" + socket.getInetAddress());
-		try {
-		BufferedReader br = getReader(socket);
-		PrintWriter pw = getWriter(socket);
-
-		pw.println("From 服务器：欢迎使用服务！");
-		pw.println("请输入用户名：");
-
-		while ((hostName=br.readLine())!=null){
-		
-		users.forEach((k,v)->{
-		if (v.equals(hostName))
-		flag =true;//线程修改了全局变量
-		});
-
-		if (!flag){
-		localName=hostName;
-		users.put(socket,hostName);
-		flag=false;
-		break;
-		}
-		else{
-		flag=false;
-		pw.println("该用户名已存在，请修改！");
-		}
-		}
-		
-		// login in   finished 
-		sendToMembers("已经上线", localName, socket);
-		pw.println("输入命令功能谢谢谢谢 ");
-		
-		String msg = null;
-		while ((msg = br.readLine()) != null) {
-			switch (msg.trim().toUpperCase()){
-				case "bye": {
-					pw.println("From 服务器：服务器已断开连接，结束服务！");
-					sendToMembers("已经下线", localName, socket);
-					System.out.println("客户端离开。");
-					break;
-					}
-				case "H":{
-					pw.println("input command function");
-					continue;
+			Boolean leave=true;
+			for(Map.Entry<Socket,String> mapEntry : users.entrySet()) {
+				if (mapEntry.getKey().equals(temp))
+					leave = false;
 				}
-				case "L":{
-					users.forEach( (k,v)->{
-						pw.println("user: "+ v); // lambda 
+			return leave;
+		}
+		
+		public void run() {//本地服务器控制台显示客户端连接的用户信息
+			System.out.println("New connection accept:" + socket.getInetAddress());
+			try {
+				BufferedReader br = getReader(socket);
+				PrintWriter pw = getWriter(socket);
+				pw.println("From 服务器：欢迎使用服务！\n 请输入用户名：" );//remind user to input username
+				while ((hostName=br.readLine())!=null){
+					users.forEach((k,v)->{
+					if (v.equals(hostName))
+						flag =true;//线程修改了全局变量
 					});
-					continue;
-				}
-				case "O":{
-					pw.println("请输入私信人的用户名：");
-					String name=br.readLine();
-					//查找map中匹配的socket，与之建立通信
-					users.forEach((k, v)->{
-					if (v.equals(name)) {
-					isExist=true;//全局变量与线程修改问题
-					}
-					});
-					//已修复用户不存在的处理逻辑
-					Socket temp=null;
-					for(Map.Entry<Socket,String> mapEntry : users.entrySet()){
-					if(mapEntry.getValue().equals(name))
-					temp = mapEntry.getKey();
-					}
-					if (isExist){
-					isExist=false;
-					//私信后有一方用户离开，另一方未知，仍然发信息而未收到回复，未处理这种情况
-					while ((msg=br.readLine())!=null){
-					if (!msg.equals("E")&&!isLeaved(temp))
-					sendToOne(msg,localName,temp);
-					else if (isLeaved(temp)){
-					pw.println("对方已经离开，已断开连接！");
-					break;
+					if (!flag){
+						localName=hostName;
+						users.put(socket,hostName);
+						flag=false;	break;
 					}
 					else{
-					pw.println("您已退出私信模式！");
-					break;
-					}
-					}
-					}
-					else
-					pw.println("用户不存在！");
-				}
-				case "G":{
-					pw.println("您已进入群聊。");
-					while ((msg=br.readLine())!=null){
-					if (!msg.equals("E")&&users.size()!=1)
-					sendToMembers(msg,localName,socket);
-					else if (users.size()==1){
-					pw.println("当前群聊无其他用户在线，已自动退出！");
-					break;
-					}
-					else {
-					pw.println("您已退出群组聊天室！");
-					break;
-					}
+						flag=false;// use flag to avoid same name
+						pw.println("该用户名已存在，请修改！");
 					}
 				}
-				default:
-					pw.print("input sum");
-			}
-	
+				sendToMembers("已经上线", localName, socket);//login in success
+				pw.println("输入命令功能谢谢谢谢 ");
+				
+				String msg = null;
+				while ((msg = br.readLine()) != null) {
+					switch (msg.trim().toUpperCase()){
+						case "BYE": {// exit
+								pw.println("From 服务器：服务器已断开连接，结束服务！");
+								sendToMembers("已经下线", localName, socket);
+								System.out.println("客户端离开。");
+								break;
+							}
+						case "H":{// show help 
+							pw.println("input command function");
+							continue;
+						}
+						case "L":{ // show all the member
+							users.forEach( (k,v)->{
+								pw.println("user: "+ v); // lambda 
+							});// print all user from the hashmap
+							continue;
+						}
+						case "O":{ // send to one 
+							pw.println("请输入私信人的用户名：");
+							String name = br.readLine();
+							//查找map中匹配的socket，与之建立通信
+							users.forEach((k, v)->{
+								if (v.equals(name)) {
+									isExist=true;//全局变量与线程修改问题
+								}
+							});
 
-		pw.println("From 服务器：" + msg);
-		pw.println("来自服务器,重复消息："+msg);
-		}
-		} catch (IOException e) {
-		e.printStackTrace();
-		} finally {
-		try {
-		if (socket != null)
-		socket.close();
-		} catch (IOException e) {
-		e.printStackTrace();
-		}
-		}
+							Socket temp = null;//已修复用户不存在的处理逻辑
+							for(Map.Entry<Socket,String> mapEntry : users.entrySet()){
+								if(mapEntry.getValue().equals(name))
+									temp = mapEntry.getKey();
+							}
+							if (isExist){
+								isExist=false;//私信后有一方用户离开，另一方未知，仍然发信息而未收到回复，未处理这种情况
+								while ((msg = br.readLine()) != null){
+									if (!msg.equals("E")&&!isLeaved(temp))
+										sendToOne(msg,localName,temp);//continue send
+									else if (isLeaved(temp)){
+										pw.println("对方已经离开，已断开连接！");
+										break;
+									}
+									else{
+										pw.println("您已退出私信模式！");
+										break;
+									}
+								}
+							}
+							else pw.println("用户不存在！");
+							break;
+						}
+						case "G":{ // group chat
+							pw.println("您已进入群聊。");
+							while ((msg=br.readLine())!=null){
+								if (!msg.equals("E")&&users.size()!=1)
+									sendToMembers(msg,localName,socket);// continue send
+								else if (users.size()==1){
+									pw.println("当前群聊无其他用户在线，已自动退出！");
+									break;
+								}
+								else {
+									pw.println("您已退出群组聊天室！");
+									break;
+								}
+							}
+						}
+						default: pw.print("please input ");
+					}
+					pw.println("From 服务器：" + msg);
+					pw.println("来自服务器,重复消息："+msg);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+				if (socket != null)
+					socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 	public void Service() throws IOException{
@@ -197,36 +178,35 @@ System.out.println("服务器启动监听在"+port+"端口...");  //constructor 
 
 		PrintWriter pw;
 		OutputStream out;
-		Iterator iterator=users.entrySet().iterator();
+		Iterator iterator = users.entrySet().iterator();
 		while (iterator.hasNext()){
-		Map.Entry entry=(Map.Entry) iterator.next();
-		Socket tempSocket = (Socket) entry.getKey();
-		String name = (String) entry.getValue();
-		if (!tempSocket.equals(mySocket)){
-		out=tempSocket.getOutputStream();
-		pw=new PrintWriter(new OutputStreamWriter(out,"utf-8"),true);
-		pw.println(hostAddress+"："+msg);
-		}
+			Map.Entry entry=(Map.Entry) iterator.next();
+			Socket tempSocket = (Socket) entry.getKey();
+			String name = (String) entry.getValue();
+			if (!tempSocket.equals(mySocket)){
+				out=tempSocket.getOutputStream();
+				pw=new PrintWriter(new OutputStreamWriter(out,"utf-8"),true);
+				pw.println(hostAddress+"："+msg);
+			}
 		}
 
 	}
+	// send to specific socket
 	private void sendToOne(String msg,String hostAddress,Socket another) throws IOException{
-
 		PrintWriter pw;
 		OutputStream out;
-
 		Iterator iterator=users.entrySet().iterator();
 		while (iterator.hasNext()){
-
-		Map.Entry entry=(Map.Entry) iterator.next();
-		Socket tempSocket = (Socket) entry.getKey();
-		String name = (String) entry.getValue();
-
-		if (tempSocket.equals(another)){
-		out=tempSocket.getOutputStream();
-		pw=new PrintWriter(new OutputStreamWriter(out,"utf-8"),true);
-		pw.println(hostAddress+"私信了你："+msg);
-		}
+	
+			Map.Entry entry=(Map.Entry) iterator.next();
+			Socket tempSocket = (Socket) entry.getKey();
+			String name = (String) entry.getValue();
+	
+			if (tempSocket.equals(another)){
+				out=tempSocket.getOutputStream();
+				pw=new PrintWriter(new OutputStreamWriter(out,"utf-8"),true);
+				pw.println(hostAddress+"私信了你："+msg);
+			}
 		}
 	}
 
